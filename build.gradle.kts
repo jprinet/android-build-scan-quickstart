d867a93b586eec374457ddaac4c7f7cfb29b1995
Jerome Prinet <jprinet@gradle.com>
2021-11-19 10:31:49 +0100

    Bump up Android to 7.0.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ buildscript {
     maven("https://plugins.gradle.org/m2/")
   }
   dependencies {
-    classpath("com.android.tools.build:gradle:4.2.0")
+    classpath("com.android.tools.build:gradle:7.0.3")
     classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.31")
     classpath("com.osacky.flank.gradle:fladle:0.17.2")
     classpath("org.jlleitschuh.gradle:ktlint-gradle:10.0.0")
--- example-app/build.gradle.kts
+++ example-app/build.gradle.kts
@@ -30,7 +30,7 @@ android {
   }
   kotlinOptions {
     val options = this as org.jetbrains.kotlin.gradle.dsl.KotlinJvmOptions
-    options.jvmTarget = "1.8"
+    options.jvmTarget = "11"
   }
 }
 
