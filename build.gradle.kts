f4e8745c929b3318e0be1d6e4366e9362e4c80f3
Jerome Prinet <jprinet@gradle.com>
2021-12-06 12:11:53 +0100

    Add realm

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,10 +5,11 @@ buildscript {
     maven("https://plugins.gradle.org/m2/")
   }
   dependencies {
-    classpath("com.android.tools.build:gradle:7.0.3")
+    classpath("com.android.tools.build:gradle:4.2.2")
     classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.31")
     classpath("com.osacky.flank.gradle:fladle:0.17.2")
     classpath("org.jlleitschuh.gradle:ktlint-gradle:10.0.0")
+    classpath("io.realm:realm-gradle-plugin:10.8.1")
   }
 }
 
