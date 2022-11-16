c8016fe07f38597d6bfee64e1b36c11fc63c0dad
Jerome Prinet <jprinet@gradle.com>
2022-11-16 15:40:24 -0500

    Update Kotlin

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ buildscript {
   }
   dependencies {
     classpath("com.android.tools.build:gradle:7.3.1")
-    classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.31")
+    classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.20")
     classpath("com.osacky.flank.gradle:fladle:0.17.2")
     classpath("org.jlleitschuh.gradle:ktlint-gradle:10.0.0")
     classpath("io.realm:realm-gradle-plugin:10.9.1-SNAPSHOT")
